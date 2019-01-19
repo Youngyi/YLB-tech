da4bcf9779d89613c07bc569875c72f065435d80
Yucheng yang <997471028@qq.com>
2019-01-19 09:57:41 +0800

    add preprocess method

--- data_preprocess/standardizing.py
+++ data_preprocess/standardizing.py
@@ -7,8 +7,14 @@ def std_to_average(df):
     return (df - df.mean()) / (df.max() - df.min())
 
 
-input_path = '/Users/yangyucheng/Desktop/SCADA/train'
-df_test_1 = pd.read_csv(listdir(input_path)[0], index_col=0)
-df_test_1 = df_test_1.drop(['ts', 'wtid'], axis=1)
-df1 = (df_test_1 - df_test_1.mean()) / (df_test_1.max() - df_test_1.min())
-print(df1.head())
+def preprocess_df(df):
+    df_part1 = df[['ts', 'wtid']]
+    df_part2 = df.drop(['ts', 'wtid'], axis=1)
+    df_part2 = (df_part2 - df_part2.mean()) / (df_part2.max() - df_part2.min())
+    df = pd.concat([df_part1, df_part2], sort=False, axis=1)
+    return df
+
+# input_path = '/Users/yangyucheng/Desktop/SCADA/train'
+# df_test_1 = pd.read_csv(listdir(input_path)[0], index_col=0)
+# df1 = preprocess_df(df_test_1)
+# print(df1.head())
